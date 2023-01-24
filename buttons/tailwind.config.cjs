3e77bd4d2a380ce51eb101d77b4ef1134b2bad8b
amanda-santos <amandasf0200@gmail.com>
2023-01-24 19:01:29 -0300

    ci: 💚 attempt to fix build

--- buttons/tailwind.config.cjs
+++ buttons/tailwind.config.cjs
@@ -2,21 +2,20 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx,svelte}"],
   theme: {
-    extend: {
-      colors: {
-        darkPurple: {
-          900: "#0B0911",
-          800: "#130F1E",
-          700: "#1C162C",
-          600: "#3C3748",
-          500: "#6A617F",
-        },
-        purple: {
-          500: "#8257E5",
-          400: "#9674E5",
-        },
+    colors: {
+      darkPurple: {
+        900: "#0B0911",
+        800: "#130F1E",
+        700: "#1C162C",
+        600: "#3C3748",
+        500: "#6A617F",
+      },
+      purple: {
+        500: "#8257E5",
+        400: "#9674E5",
       },
     },
+    extend: {},
   },
   plugins: [],
 };
