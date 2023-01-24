ea6730c8c293284bf50a4b985fb924adec94007f
amanda-santos <amandasf0200@gmail.com>
2023-01-24 19:07:32 -0300

    Revert "ci: 💚 attempt to fix build"
    This reverts commit 3e77bd4d2a380ce51eb101d77b4ef1134b2bad8b.

--- buttons/tailwind.config.cjs
+++ buttons/tailwind.config.cjs
@@ -2,20 +2,21 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx,svelte}"],
   theme: {
-    colors: {
-      darkPurple: {
-        900: "#0B0911",
-        800: "#130F1E",
-        700: "#1C162C",
-        600: "#3C3748",
-        500: "#6A617F",
-      },
-      purple: {
-        500: "#8257E5",
-        400: "#9674E5",
+    extend: {
+      colors: {
+        darkPurple: {
+          900: "#0B0911",
+          800: "#130F1E",
+          700: "#1C162C",
+          600: "#3C3748",
+          500: "#6A617F",
+        },
+        purple: {
+          500: "#8257E5",
+          400: "#9674E5",
+        },
       },
     },
-    extend: {},
   },
   plugins: [],
 };
